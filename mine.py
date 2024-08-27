0062c93e6b0d6770f3154dc472e086f827179010
QuinOG <quinlandavis0010@gmail.com>
2024-08-26 20:01:27 -0400

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,7 +1,5 @@
-print ("Hello world!")
-
 def hello_world():
   # Create a variable that has the value of "Hello world!" so that the function will return the appropriate value.
   mine = print("Hello world!")
   return mine
-hello_world()
+print ("Hello world!")
